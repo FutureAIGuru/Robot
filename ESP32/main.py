f5821252ae0296d11e97553a901124fd72b132b0
Andre Slabber <andre.slabber@moorelife.nl>
2021-09-19 16:34:01 +0200

    OTAUpdater experiment

---
+++ ESP32/main.py
@@ -0,0 +1,37 @@
+import ujson
+import utime
+
+from ota_update.main.ota_updater import OTAUpdater
+
+@staticmethod
+def _otaUpdate():
+    from .ota_updater import OTAUpdater
+    otaUpdater = OTAUpdater('https://github.com/FutureAIGuru/Robot', github_src_dir='ESP32', main_dir='app', secrets_file="config.json")
+    otaUpdater.install_update_if_available()
+    del(otaUpdater)
+
+def download_and_install_update_if_available(config_data):
+    if 'wifi' in config_data:
+        o = OTAUpdater('https://github.com/FutureAIGuru/Robot')
+        o.download_and_install_update_if_available(config_data['wifi']['ssid'], config_data['wifi']['password'])
+    else:
+        print('No WIFI configured, skipping updates check')
+
+
+def start(config_data):
+    global s
+    utime.sleep_ms(10000)
+    from main.RobotBrain import RobotBrain
+    s = RobotBrain(config_data)
+
+
+def boot_RobotBrain():
+    f = open('config.json')
+    config_data = ujson.load(f)
+
+    download_and_install_update_if_available(config_data)
+    start(config_data)
+
+
+s = None
+boot_RobotBrain()
